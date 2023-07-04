6643c0a914deb4da6a0cc7c812d0c1134fba42be
Rafał Kowalski <rafkow91@yahoo.com>
2023-07-04 18:47:22 +0200

    complete basic bot function - getting tokens from csv

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+'''
+Bot to manage discord server - can add roles by using token.
+
+All commands are written in Polish because bot is used on Polish discord server.
+All operations are logging into "DoKoduBot_test.log.txt"
+'''
 from os import environ, path
 import logging
 import csv
@@ -7,7 +13,7 @@ from discord.ext import commands
 
 from dotenv import load_dotenv
 
-from utils import generate_token
+from utils import generate_token, get_role_by_token_from_csv
 
 
 ROLES = {
@@ -15,9 +21,12 @@ ROLES = {
     'pyrest': 'Test2',
 }
 
-logging.basicConfig(level=logging.INFO, filename='DoKoduBot_test.log', encoding='utf8', format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %I:%M:%S')
 load_dotenv()
 DISCORD_TOKEN = environ.get('DISCORD_TOKEN')
+LOG_FILENAME = environ.get('LOG_FILENAME', 'DoKoduBot_test.log')
+
+logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, encoding='utf8',
+                    format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %I:%M:%S')
 
 intents = discord.Intents.all()
 
@@ -30,24 +39,46 @@ async def on_ready():
 
 
 @client.command(name='kursant')
-async def add_trainee(message):
-    member = message.author
-    logging.info(f'Start adding new trainee - {member.name}')
-    role = discord.utils.get(member.guild.roles, name='Test1')
-    await member.add_roles(role)
-    await member.send(f'Teraz już masz nową rolę - {role.name}')
-    logging.info(f'Adding complete\nMember: {member}\nRole: {role.name}')
+async def add_trainee(message, token=None):
+    ''' Add role for user using token.
+    After complete the adding role process bot send PM to user and inform her/him
+
+    Arguments:
+        message -- command name
+        token -- token to add specific role
+    '''
+    if token is not None:
+        member = message.author
+        course = get_role_by_token_from_csv(token, './codes.csv')
+        role_name = ROLES.get(course)
+        if role_name is not None:
+            logging.info(f'Start adding new trainee - {member.name}')
+            role = discord.utils.get(member.guild.roles, name=role_name)
+            await member.add_roles(role)
+            await member.send(f'Teraz już masz nową rolę - {role.name}')
+            logging.info(
+                f'Adding complete | Member: {member} | Role: {role.name}')
 
 
 @client.command(name='generate_codes')
 @commands.has_permissions(administrator=True)
-async def generate_codes(message, course, counter:int = 1):
+async def generate_codes(message, course, counter: int = 1):
+    ''' Prepare tokens to send to users
+
+    Arguments:
+        message -- command name
+        course -- name of course for which you need tokens - courses are symbolic names of roles
+
+    Keyword Arguments:
+        counter -- how many tokens you want to prepare? (default: {1})
+    '''
     logging.info(f'generate {counter} new codes by {message.author.name}')
     for i in range(counter):
         if not path.exists('codes.csv'):
             with open('codes.csv', mode='w', encoding='utf8') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow(['token', 'course', 'used', 'used_date', 'used_by',])
+                writer.writerow(
+                    ['token', 'course', 'used', 'used_date', 'used_by',])
         with open('codes.csv', mode='a', encoding='utf8') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import csv
+import logging
 from string import ascii_letters, digits
 from random import randint
 
@@ -8,3 +10,20 @@ def generate_token(lenght: int = 12) -> str:
     for i in range(lenght):
         token += CHARS[randint(0, len(CHARS)-1)]
     return token
+
+def get_role_by_token_from_csv(token: str, csv_token_file: str) -> str:
+    try:
+        with open(csv_token_file, encoding='utf8') as token_file:
+            tokens = list(csv.DictReader(token_file))
+    except FileNotFoundError as err:
+        logging.error(err)
+        return None
+
+    tokens_list = [line['token'] for line in tokens]
+    
+    try:
+        line = tokens[tokens_list.index(token)]
+        return line['course']
+    except ValueError as err:
+        logging.error(err)
+        return None
