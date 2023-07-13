cda585c7cb98c70b9921e8f4b7b00f6a72d50683
Rafał Kowalski <rafkow91@yahoo.com>
2023-07-13 18:49:08 +0200

    add docs to database.py

--- database/database.py
+++ database/database.py
@@ -1,10 +1,17 @@
+''' Context manager to manage db '''
 from datetime import datetime
 import logging
 import os
 import sqlite3
 
+# pylint: disable=logging-fstring-interpolation
+# pylint: disable=unspecified-encoding
+# pylint: disable=inconsistent-return-statements
+
 
 class Database:
+    ''' Class to manage db as context manager '''
+
     def __init__(self, db_path: str = None) -> None:
         if db_path is None:
             db_path = './database.db'
@@ -17,17 +24,32 @@ class Database:
         self.cursor = self.connection.cursor()
         self.sql_scripts = './database/sql'
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            logging.error(exc_val)
+
     def prepare_db(self) -> None:
+        ''' Function to prepare db described in 01_initial.sql file,
+        operational errors are logging '''
         with open('./database/sql/01_initial.sql') as script_file:
             script = script_file.read()
 
         try:
             self.cursor.execute(script)
-            logging.info(f'Created table \'tokens\'')
+            logging.info('Created table \'tokens\'')
         except sqlite3.OperationalError as err:
             logging.error(f'01_initial.sql: {err}')
 
     def save_token(self, token: str, course: str) -> None:
+        ''' Function to save tokens into db - base on 02_add_token.sql script,
+        operational and integrity errors are logging
+
+        Arguments:
+            token -- string which will be add to db
+            course -- course to which the given token is assigned '''
         with open('./database/sql/02_add_token.sql') as script_file:
             script = script_file.read()
 
@@ -42,6 +64,18 @@ class Database:
             return 'Error'
 
     def use_token(self, token: str, user: str) -> tuple:
+        ''' Function to marking token as used,
+        operational errors are logging
+
+        Arguments:
+            token -- string which will be check into db and mark as used
+            user -- member who used the token
+
+        Returns:
+            tuple (row from db) if all went well
+                or None if token don't exist, has used before
+                or except operational error
+        '''
         data = self.get_token(token)
         if data is None:
             logging.warning(f'Token {token} not exist in DB | user: {user}')
@@ -60,9 +94,19 @@ class Database:
                 logging.error(f'04_mark_token_as_used.sql: {err}')
                 return None
 
-        logging.error(f'Token {token} is used by {data[4]} (date: {data[5]}) | now try to use by {user}')
+        logging.error(
+            f'Token {token} is used by {data[4]} (date: {data[5]}) | now try to use by {user}')
 
     def get_token(self, token: str) -> tuple:
+        ''' Function to getting row from db searched by token
+
+        Arguments:
+            token -- string which will be check into db and mark as used
+
+        Returns:
+            tuple (row from db) if all went well
+                or None if token don't exist or except operational error
+        '''
         with open('./database/sql/03_check_token.sql') as script_file:
             script = script_file.read()
 
@@ -71,3 +115,4 @@ class Database:
             return self.cursor.fetchone()
         except sqlite3.OperationalError as err:
             logging.error(f'03_check_token.sql: {err}')
+    
